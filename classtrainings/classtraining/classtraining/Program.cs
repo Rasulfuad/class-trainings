05ec610784197bcbcbb6b5482b004f27fae70784
Rasulfuad <fuadar@code.edu.az>
2022-03-16 17:45:01 +0400

    feat:add class

---
+++ classtrainings/classtraining/classtraining/Program.cs
@@ -0,0 +1,40 @@
+﻿using classtraining.models;
+using System;
+
+
+namespace classtraining
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person pp1 = new Person("alim", 25, 26);
+            pp1.name = "Fuad";
+            pp1.surname = "Rasulzade";
+            pp1.age = 28;
+            pp1.weight = 75;
+            pp1.profession = "sales";
+            pp1.fullname();
+            pp1.wght();
+
+
+         //   Console.WriteLine(pp1.wght());
+
+
+
+
+
+
+
+
+
+        }
+    }
+
+
+}
+
+
+
+
+
---
+++ classtrainings/classtraining/classtraining/models/Person.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace classtraining.models
+{
+    internal class Person
+    {
+
+
+        public string name;
+        public string surname;
+        public int age;
+        public int weight;
+        public string profession;
+
+        public void fullname()
+        {
+            Console.WriteLine($"{name} { surname} ");
+
+        }
+        public int wght()
+        {
+
+            return weight;
+        }
+        public Person(string name) 
+        {
+            this.name = name;
+        }
+        public Person(string name, int age, int weight):this(name)
+        {
+            
+            this.age = age;
+            this.weight = weight;
+        }
+
+    }
+}
+
+
